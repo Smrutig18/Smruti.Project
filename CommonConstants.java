9b36fa66f4ed04fc0b6e6a579d7f12a25d0a147b
Smrutig18 <156103035+Smrutig18@users.noreply.github.com>
2024-01-09 20:40:53 +0530

    Add files via upload

---
+++ CommonConstants.java
@@ -0,0 +1,12 @@
+package CommonData;
+
+public class CommonConstants {
+
+	public static final String URLValue = "https://opensource-demo.orangehrmlive.com/";
+	public static final String cityDistrictIndiaData = "Mumbai";
+	public static final String valueFour = "2";
+	public static final String rooms = "Rooms";
+	public static final String adults = "Adults";
+	public static final String children = "Children";
+	
+}
